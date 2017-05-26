1e16f318899fe7514f3d3ad11a5f2c35528d0472
Raman Maan <maanr@myumanitoba.ca>
2017-05-26 16:26:28 -0500

    A better way to handle snake canvas creation
    Now append and remove snake canvas as needed so it's not running in the background

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-var paused = true;
-var alreadyCreated = false;//band-aid solution, will implement a better one
-
 $(document).ready(function(){
   $('.play').click(function() {
-    if(!alreadyCreated) {
-      alreadyCreated = true;
+    if($('#snake-canvas').length) {
+      //already exists, so we should end game
+      $('#snake-canvas').remove();
+    } else {
+      //we should create the snake game canvas
+      var canvas = document.createElement('canvas');
+      $('#game-area').append("<canvas id=\"snake-canvas\"></canvas>");
       playSnake();
     }
-    paused = !paused;
-    $('.game-area').toggleClass('invisible');
   });
 });
