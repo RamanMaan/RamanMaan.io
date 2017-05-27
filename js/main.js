9d7eca5d89ef21eee1bab56b9309eaef247db405
Raman Maan <maanr@myumanitoba.ca>
2017-05-27 15:13:08 -0500

    Added a blur effect to background during game

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
-$(document).ready(function(){
-  $('.play').click(function() {
-    //swap play button and header text depending on game state
-    flipPlayElements();
-    $('#score').toggleClass('hidden')
-
-    //handle the snake canvas
-    if($('#snake-canvas').length) {
-      //already exists, so we should end game
-      $('#snake-canvas').remove();
-    } else {
-      //we should create the snake game canvas
-      var canvas = document.createElement('canvas');
-      $('#game-area').append("<canvas id=\"snake-canvas\"></canvas>");
-      playSnake();
-    }
-  });
+$(document).ready(function () {
+	$('.play').click(function () {
+		//swap play button and header text depending on game state
+		flipPlayElements();
+		setPlayArea();
+		//handle the snake canvas
+		if ($('#snake-canvas').length) {
+			//already exists, so we should end game
+			$('#snake-canvas').remove();
+		} else {
+			//we should create the snake game canvas
+			var canvas = document.createElement('canvas');
+			$('#game-area').append('<canvas id="snake-canvas"></canvas>');
+			playSnake();
+		}
+	});
 });
 
 function flipPlayElements() {
-  $('#play').text(function(i, oldText) {
-    return oldText === 'Play' ? 'End' : 'Play'
-  });
-  var src = $('#side-play').attr('src') === 'images/play.png' ? 'images/cancel.png' : 'images/play.png';
-  $('#side-play').attr('src', src);
+	$('#play').text(function (i, oldText) {
+		return oldText === 'Play' ? 'End' : 'Play';
+	});
+	var src = $('#side-play').attr('src') === 'images/play.png' ? 'images/cancel.png' : 'images/play.png';
+	$('#side-play').attr('src', src);
+}
+
+function setPlayArea() {
+	//let score appear
+	$('#score').toggleClass('hidden');
+
+	//blur background
+	$('#container').toggleClass('blur');
 }
 
 function comingSoon() {
-  alert("Coming soon!");
+	alert('Coming soon!');
 }
