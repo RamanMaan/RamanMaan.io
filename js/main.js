6e1d8aca313558021e580d39e38c3bb8e52273b6
Raman Maan <maanr@myumanitoba.ca>
2017-06-08 17:59:00 -0500

    Adding a typed jumbotron
    Removed sidebar
    Added a jumbotron that's typed in using Typed.js library

--- js/main.js
+++ js/main.js
@@ -1,42 +1,13 @@
 $(document).ready(function () {
-	$('.play').click(function () {
-		//swap play button and header text depending on game state
-		flipPlayElements();
-		setPlayArea();
-		//handle the snake canvas
-		if ($('#snake-canvas').length) {
-			//already exists, so we should end game
-			$('#snake-canvas').fadeRemove(200);
-		} else {
-			//we should create the snake game canvas
-			$('<canvas id="snake-canvas"></canvas>').hide().appendTo('#game-area').fadeIn(300);
-			playSnake();
-		}
+	// begin the typing animation
+	$(function () {
+		$('#typed-header').typed({
+			strings: ['<small>Programmer.^1000 Nerd.^1000 Kind of cool guy.^1000</small>', 'Raman <strong>Maan</strong>'],
+			typeSpeed: 3,
+			startDelay: 800,
+			backSpeed: 0.5,
+			backDelay: 650,
+			showCursor: false
+		});
 	});
 });
-
-function flipPlayElements() {
-	$('#play').text(function (i, oldText) {
-		return oldText === 'Play' ? 'End' : 'Play';
-	});
-	var src = $('#side-play').attr('src') === 'images/play.png' ? 'images/cancel.png' : 'images/play.png';
-	$('#side-play').attr('src', src);
-}
-
-function setPlayArea() {
-	//let score appear
-	$('#score').toggleClass('hidden');
-
-	//blur background
-	$('#container').toggleClass('blur');
-}
-
-function comingSoon() {
-	alert('Coming soon!');
-}
-
-jQuery.fn.fadeRemove = function(speed) {
-	$(this).fadeOut(speed, function() {
-		$(this).remove();
-	})
-}
