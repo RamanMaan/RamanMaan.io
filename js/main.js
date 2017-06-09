c3cb8c0c12da85851bba435011bb7441248c5aa0
Raman Maan <maanr@myumanitoba.ca>
2017-06-08 22:30:24 -0500

    Reorganized styles, added Sass, added preloader
    Created a new organization method for styles, instead of css now contained in styles directory. One main.css is generated with all scss files

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-$(document).ready(function () {
+$(document).ready( function () {
+	clearPreloader();
 	// begin the typing animation
-	$(function () {
-		$('#typed-header').typed({
-			strings: ['<small>Programmer.^1000 Nerd.^1000 Kind of cool guy.^1000</small>', 'Raman <strong>Maan</strong>'],
-			typeSpeed: 3,
-			startDelay: 800,
-			backSpeed: 0.5,
-			backDelay: 650,
-			showCursor: false
-		});
-	});
+	typeHeader();
 });
+
+function typeHeader() {
+	$('#typed-header').typed({
+		strings: ['<small>Programmer.^1000 Nerd.^1000 Kind of cool guy.^1000</small>', 'Raman <strong>Maan</strong>'],
+		typeSpeed: 3,
+		startDelay: 3000,
+		backSpeed: 0.5,
+		backDelay: 650,
+		showCursor: false
+	});
+}
---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+function clearPreloader() {
+	setTimeout(function () {
+		$('#preloader-wrapper').fadeOut();
+	}, 2000);
+}
