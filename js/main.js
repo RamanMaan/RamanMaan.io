922160262e213873127021c6b31187aeeb2c2e54
Raman Maan <maanr@myumanitoba.ca>
2017-07-17 15:49:57 -0500

    Slight change to how preloader fades out
    It took too long to time out

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function typeHeader() {
 	$('#typed-header').typed({
 		strings: ['<small>Programmer.^1000 Nerd.^1000 Kind of cool guy.^1000</small>', 'Raman <strong>Maan</strong>'],
 		typeSpeed: 3,
-		startDelay: 3000,
+		startDelay: 1500,
 		backSpeed: 0.5,
 		backDelay: 650,
 		showCursor: true
--- js/preloader.js
+++ js/preloader.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
 	setTimeout(function () {
 		$('#preloader-wrapper').fadeOut();
-	}, 2000);
+	}, 100);
 });
