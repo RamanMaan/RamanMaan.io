cb208afee3f15cff65d51d991230202a00e15cbb
Raman Maan <raman_maan@hotmail.com>
2017-12-29 22:53:02 -0600

    Removing old site contents

--- cole/js/landing.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-	if ($(".navbar").offset().top > 50) {
-		$('.navbar-transparent').addClass('affix');
-		$(".navbar-fixed-top").addClass("top-nav-collapse");
-	} else {
-		$('.navbar-transparent').removeClass('affix');
-		$(".navbar-fixed-top").removeClass("top-nav-collapse");
-	}
-});
--- cole/js/main.js
+++
@@ -1,5 +0,0 @@
-window.sr = ScrollReveal();
-
-var section_config = {duration: 600, delay: 20};
-sr.reveal('#home');
-sr.reveal('#portfolio', section_config);
